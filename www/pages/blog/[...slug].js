16c691fb86e5db2184d7d3f59aa8781b3b13acfa
oysi <oysi93@gmail.com>
2022-03-04 02:10:40 +0100

    minor changes

--- www/pages/blog/[...slug].js
+++ www/pages/blog/[...slug].js
@@ -1,16 +1,20 @@
 
 import { useRouter } from "next/router";
 
-export default function Index(props) {
-	console.log(props);
+export default function Index() {
+	// console.log(props);
 	const router = useRouter();
-	const { slug } = router.query;
+	// const { slug } = router.query;
+	const query = router.query;
+	const [year, month, name] = query?.slug ?? [];
 	
-	console.log("slug", slug);
+	// console.log("slug", slug);
 	
 	return (
 		<div>
-			{slug.join(" ")}
+			<p>{year}</p>
+			<p>{month}</p>
+			<p>{name}</p>
 		</div>
 	)
 }
